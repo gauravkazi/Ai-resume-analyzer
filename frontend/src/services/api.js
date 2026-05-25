8ae08ae59efd0d732a696018a3a496bcc91feb85
Gaurav thapa <gauravkazi@icloud.com>
2026-05-25 14:52:14 +0545

    fix API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ai-resume-analyzer-koea.onrender.com",
+  baseURL: "https://ai-resume-analyzer-koea.onrender.com/api",
 });
 
 export default API;
